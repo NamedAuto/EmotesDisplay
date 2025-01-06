db20a29d935a4c47437736bc02f9104b0bd7b4ae
NamedAuto <ozmartinez@tutanota.com>
2025-01-06 00:39:39 -0800

    Settings page ui finished. Still need to handle saving changes

--- frontend/src/components/Settings/Settings.tsx
+++ frontend/src/components/Settings/Settings.tsx
@@ -15,81 +15,123 @@ import { useConfig } from "../Config/ConfigProvider";
 const SettingsPage: React.FC = () => {
   const config = useConfig();
 
-  // Youtube
-  const [apiKey, setApiKey] = useState<string>(config.Youtube.ApiKey);
-  const [videoId, setVideoId] = useState<string>(config.Youtube.VideoId);
-  const [messageDelay, setMessageDelay] = useState<string>(
-    config.Youtube.MessageDelay.toString()
-  );
+  const [settings, setSettings] = useState({
+    apiKey: config.Youtube.ApiKey,
+    videoId: config.Youtube.VideoId,
+    messageDelay: config.Youtube.MessageDelay.toString(),
+    port: config.Port.toString(),
+    forceWidthHeight: config.AspectRatio.ForceWidthHeight,
+    canvasWidth: config.AspectRatio.Width.toString(),
+    canvasHeight: config.AspectRatio.Height.toString(),
+    scaleCanvas: config.AspectRatio.ScaleCanvas.toString(),
+    scaleImage: config.AspectRatio.ScaleImage.toString(),
+    emoteHeight: config.Emote.Height.toString(),
+    emoteWidth: config.Emote.Width.toString(),
+    randomSizeIncrease: config.Emote.RandomSizeIncrease.toString(),
+    randomSizeDecrease: config.Emote.RandomSizeDecrease.toString(),
+    maxEmoteCount: config.Emote.MaxEmoteCount.toString(),
+    emoteRoundness: config.Emote.Roundness.toString(),
+    emoteBackgroundColor: config.Emote.BackgroundColor,
+    test: config.Testing.Test,
+    speedOfEmotes: config.Testing.SpeedOfEmotes.toString(),
+  });
 
-  // Port
-  const [port, setPort] = useState<string>(config.Port.toString());
+  const handleReset = () => {
+    setSettings({
+      apiKey: config.Youtube.ApiKey,
+      videoId: config.Youtube.VideoId,
+      messageDelay: config.Youtube.MessageDelay.toString(),
+      port: config.Port.toString(),
+      forceWidthHeight: config.AspectRatio.ForceWidthHeight,
+      canvasWidth: config.AspectRatio.Width.toString(),
+      canvasHeight: config.AspectRatio.Height.toString(),
+      scaleCanvas: config.AspectRatio.ScaleCanvas.toString(),
+      scaleImage: config.AspectRatio.ScaleImage.toString(),
+      emoteHeight: config.Emote.Height.toString(),
+      emoteWidth: config.Emote.Width.toString(),
+      randomSizeIncrease: config.Emote.RandomSizeIncrease.toString(),
+      randomSizeDecrease: config.Emote.RandomSizeDecrease.toString(),
+      maxEmoteCount: config.Emote.MaxEmoteCount.toString(),
+      emoteRoundness: config.Emote.Roundness.toString(),
+      emoteBackgroundColor: config.Emote.BackgroundColor,
+      test: config.Testing.Test,
+      speedOfEmotes: config.Testing.SpeedOfEmotes.toString(),
+    });
+  };
 
-  // Aspect Ratio
-  const [forceWidthHeight, setForcedWidthHeight] = useState<boolean>(
-    config.AspectRatio.ForceWidthHeight
-  );
-  const [canvasWidth, setCanvasWidth] = useState<string>(
-    config.AspectRatio.Width.toString()
-  );
-  const [canvasHeight, setCanvasHeight] = useState<string>(
-    config.AspectRatio.Height.toString()
-  );
-
-  const [scaleCanvas, setScaleCanvase] = useState<string>(
-    config.AspectRatio.ScaleCanvas.toString()
-  );
-  const [scaleImage, setScaleImage] = useState<string>(
-    config.AspectRatio.ScaleImage.toString()
-  );
-
-  // Emote
-  const [emoteHeight, setEmoteHeight] = useState<string>(
-    config.Emote.Height.toString()
-  );
-  const [emoteWidth, setEmoteWidth] = useState<string>(
-    config.Emote.Width.toString()
-  );
-  const [randomSizeIncrease, setRandomSizeIncrease] = useState<string>(
-    config.Emote.RandomSizeIncrease.toString()
-  );
-  const [randomSizeDecrease, setRandomSizeDecrease] = useState<string>(
-    config.Emote.RandomSizeDecrease.toString()
-  );
-  const [maxEmoteCount, setMaxEmoteCount] = useState<string>(
-    config.Emote.MaxEmoteCount.toString()
-  );
-  const [roundness, setRoundness] = useState<string>(
-    config.Emote.Roundness.toString()
-  );
-  const [backgroundColor, setBackgroundColor] = useState<string>(
-    config.Emote.BackgroundColor
-  );
-
-  // Testing
-  const [test, setTest] = useState<boolean>(config.Testing.Test);
-  const [speedOfEmotes, setSpeedOfEmotes] = useState<string>(
-    config.Testing.SpeedOfEmotes.toString()
-  );
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value, type, checked } = event.target;
+    setSettings((prevValues) => ({
+      ...prevValues,
+      [name]: type === "checkbox" ? checked : value,
+    }));
+  };
 
   const handleSave = () => {
     console.log("Settings saved");
   };
+
+  /*
+  const handleSave = () => {
+    console.log("Settings saved");
+    // Update config and notify backend
+    config.update(formValues);
+
+    fetch('/api/config', {
+      method: 'POST', // or PUT depending on your API design
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formValues),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Settings updated successfully:', data);
+      })
+      .catch(error => console.error('Error updating settings:', error));
+  };
+*/
+
+  // #62B0A6
+  // #960018
+  // #e9325e
+  // #f885c0
+
+  // #aa1241
+  // #c0022d
+  // #c7002e
+  // #c2002b
+
+  // #d39059
+  // #f1f6fc
+  //rgb(241, 246, 252)
+
+  //rgb(22, 54, 50)
+  //rgb(37, 94, 86)
+  // #224a44
+
+  //rgb(98, 176, 159)
+  // #6c072c
 
   // Create a custom theme for the Settings component
   const darkTheme = createTheme({
     palette: {
       mode: "dark",
       background: {
-        // rgb(20, 40, 48)
+        // #142830
         default: "#142830",
       },
       primary: {
-        // rgb(241, 246, 252)
-        main: "#f1f6fc",
+        // #f885c0
+        main: "#f885c0",
+      },
+      secondary: {
+        // #62B0A6
+        main: "#62B0A6",
       },
       text: {
-        primary: "#f1f6fc",
+        // #e40031
+        primary: "#e40031",
       },
     },
     components: {
@@ -97,16 +139,20 @@ const SettingsPage: React.FC = () => {
         styleOverrides: {
           root: {
             "& .MuiInputBase-input": {
-              color: "#f1f6fc", // Text color inside the input
+              // #f4458f
+              color: "#f4458f", // Text color inside the input
             },
             "& .MuiInputLabel-root": {
-              color: "#f1f6fc", // Label color
+              // #f885c0
+              color: "#f885c0", // Label color
             },
             "& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
-              borderColor: "#f1f6fc", // Outline color
+              // #62B0A6
+              borderColor: "#62B0A6", // Outline color
             },
             "&:hover .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
-              borderColor: "#f1f6fc", // Outline color on hover
+              // #e40031
+              borderColor: "#e40031", // Outline color on hover
             },
           },
         },
@@ -125,175 +171,245 @@ const SettingsPage: React.FC = () => {
           flexDirection: "column",
           alignItems: "center",
           justifyContent: "center",
-          padding: 2,
           width: "100%",
         }}
       >
-        <Grid2>
-          <Typography variant="h4" gutterBottom>
-            Youtube
-          </Typography>
+        <Typography variant="h4" gutterBottom>
+          Youtube
+        </Typography>
+        <Box>
           <TextField
+            name="apiKey"
             label="Api Key"
-            value={apiKey}
-            onChange={(e) => setApiKey(e.target.value)}
+            value={settings.apiKey}
+            onChange={handleInputChange}
             margin="normal"
+            sx={{ marginLeft: 2, marginRight: 2 }}
           />
           <TextField
+            name="videoId"
             label="Video Id"
-            value={videoId}
-            onChange={(e) => setVideoId(e.target.value)}
+            value={settings.videoId}
+            onChange={handleInputChange}
             margin="normal"
+            sx={{ marginLeft: 2, marginRight: 2 }}
           />
           <TextField
+            name="messageDelay"
             label="Message Delay"
-            value={messageDelay}
-            onChange={(e) => setMessageDelay(e.target.value)}
+            value={settings.messageDelay}
+            onChange={handleInputChange}
             type="number"
             margin="normal"
+            sx={{ marginLeft: 2, marginRight: 2 }}
           />
-        </Grid2>
+        </Box>
 
-        <Grid2>
-          <Typography variant="h4" gutterBottom>
-            Port
-          </Typography>
+        <Typography variant="h4" gutterBottom sx={{ marginTop: 4 }}>
+          Port
+        </Typography>
+        <Box>
           <TextField
+            name="port"
             label="Port"
-            value={port}
-            onChange={(e) => setPort(e.target.value)}
+            value={settings.port}
+            onChange={handleInputChange}
             type="number"
             margin="normal"
           />
-        </Grid2>
+        </Box>
 
-        <Grid2>
-          <Typography variant="h4" gutterBottom>
-            Aspect Ratio
-          </Typography>
+        <Typography variant="h4" gutterBottom sx={{ marginTop: 4 }}>
+          Aspect Ratio
+        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
           <FormControlLabel
             control={
               <Checkbox
-                checked={forceWidthHeight}
-                onChange={(e) => setForcedWidthHeight(e.target.checked)}
+                name="forceWidthHeight"
+                checked={settings.forceWidthHeight}
+                onChange={handleInputChange}
+                // sx={{
+                //   "& .MuiSvgIcon-root": {
+                //     border: settings.test ? "2px solid #f885c0" : "none",
+                //     borderRadius: "4px",
+                //   },
+                // }}
               />
             }
             label="Force Width Height"
+            sx={{ marginLeft: 2, marginRight: 2 }}
           />
           <TextField
+            name="canvasWidth"
             label="Width"
-            value={canvasWidth}
-            onChange={(e) => setCanvasWidth(e.target.value)}
+            value={settings.canvasWidth}
+            onChange={handleInputChange}
             type="number"
             margin="normal"
+            sx={{ marginLeft: 2, marginRight: 2 }}
           />
           <TextField
+            name="canvasHeight"
             label="Height"
-            value={canvasHeight}
-            onChange={(e) => setCanvasHeight(e.target.value)}
+            value={settings.canvasHeight}
+            onChange={handleInputChange}
             type="number"
             margin="normal"
+            sx={{ marginLeft: 2, marginRight: 2 }}
           />
           <TextField
+            name="scaleCanvas"
             label="Scale Canvas"
-            value={scaleCanvas}
-            onChange={(e) => setScaleCanvase(e.target.value)}
+            value={settings.scaleCanvas}
+            onChange={handleInputChange}
             type="number"
             margin="normal"
+            sx={{ marginLeft: 2, marginRight: 2 }}
           />
           <TextField
+            name="scaleImage"
             label="Scale Image"
-            value={scaleImage}
-            onChange={(e) => setScaleImage(e.target.value)}
+            value={settings.scaleImage}
+            onChange={handleInputChange}
             type="number"
             margin="normal"
+            sx={{ marginLeft: 2, marginRight: 2 }}
           />
-        </Grid2>
+        </Box>
 
-        <Grid2>
-          <Typography variant="h4" gutterBottom>
-            Emote
-          </Typography>
+        <Typography variant="h4" gutterBottom sx={{ marginTop: 4 }}>
+          Emote
+        </Typography>
+        <Box>
           <TextField
+            name="emoteWidth"
             label="Emote Width"
-            value={emoteWidth}
-            onChange={(e) => setEmoteWidth(e.target.value)}
+            value={settings.emoteWidth}
+            onChange={handleInputChange}
             type="number"
             margin="normal"
+            sx={{ marginLeft: 2, marginRight: 2 }}
           />
           <TextField
+            name="emoteHeight"
             label="Emote Height"
-            value={emoteHeight}
-            onChange={(e) => setEmoteHeight(e.target.value)}
+            value={settings.emoteHeight}
+            onChange={handleInputChange}
             type="number"
             margin="normal"
+            sx={{ marginLeft: 2, marginRight: 2 }}
           />
           <TextField
+            name="randomSizeIncrease"
             label="Random Size Increase"
-            value={randomSizeIncrease}
-            onChange={(e) => setRandomSizeIncrease(e.target.value)}
+            value={settings.randomSizeIncrease}
+            onChange={handleInputChange}
             type="number"
             margin="normal"
+            sx={{ marginLeft: 2, marginRight: 2 }}
           />
           <TextField
+            name="randomSizeDecrease"
             label="Random Size Decrease"
-            value={randomSizeDecrease}
-            onChange={(e) => setRandomSizeDecrease(e.target.value)}
+            value={settings.randomSizeDecrease}
+            onChange={handleInputChange}
             type="number"
             margin="normal"
+            sx={{ marginLeft: 2, marginRight: 2 }}
           />
           <TextField
+            name="maxEmoteCount"
             label="Max Emote Count"
-            value={maxEmoteCount}
-            onChange={(e) => setMaxEmoteCount(e.target.value)}
+            value={settings.maxEmoteCount}
+            onChange={handleInputChange}
             type="number"
             margin="normal"
+            sx={{ marginLeft: 2, marginRight: 2 }}
           />
           <TextField
+            name="emoteRoundness"
             label="Roundness"
-            value={roundness}
-            onChange={(e) => setRoundness(e.target.value)}
+            value={settings.emoteRoundness}
+            onChange={handleInputChange}
             type="number"
             margin="normal"
+            sx={{ marginLeft: 2, marginRight: 2 }}
           />
           <TextField
+            name="emoteBackgroundColor"
             label="Background Color"
-            value={backgroundColor}
-            onChange={(e) => setBackgroundColor(e.target.value)}
+            value={settings.emoteBackgroundColor}
+            onChange={handleInputChange}
             margin="normal"
+            sx={{ marginLeft: 2, marginRight: 2 }}
           />
-        </Grid2>
+        </Box>
 
-        <Grid2>
-          <Typography variant="h4" gutterBottom>
-            Testing
-          </Typography>
+        <Typography variant="h4" gutterBottom sx={{ marginTop: 4 }}>
+          Testing
+        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
           <FormControlLabel
             control={
               <Checkbox
-                checked={test}
-                onChange={(e) => setTest(e.target.checked)}
+                name="test"
+                checked={settings.test}
+                onChange={handleInputChange}
+                // sx={{
+                //   "& .MuiSvgIcon-root": {
+                //     border: settings.test ? "2px solid #f885c0" : "none",
+                //     borderRadius: "4px",
+                //   },
+                // }}
               />
             }
             label="Test"
+            sx={{ marginLeft: 2, marginRight: 2 }}
           />
           <TextField
+            name="speedOfEmotes"
             label="Speed Of Emotes"
-            value={speedOfEmotes}
-            onChange={(e) => setSpeedOfEmotes(e.target.value)}
+            value={settings.speedOfEmotes}
+            onChange={handleInputChange}
             type="number"
             margin="normal"
+            sx={{ marginLeft: 2, marginRight: 2 }}
           />
-        </Grid2>
+        </Box>
 
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={handleSave}
-          style={{ marginTop: "20px" }}
-        >
-          Save Settings
-        </Button>
+        <Box sx={{ marginTop: 4 }}>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleSave}
+            style={{ marginTop: "20px" }}
+            sx={{ marginLeft: 2, marginRight: 2, width: "150px" }}
+          >
+            Save
+          </Button>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={handleReset}
+            style={{ marginTop: "20px" }}
+            sx={{ marginLeft: 2, marginRight: 2, width: "150px" }}
+          >
+            Reset
+          </Button>
+        </Box>
       </Box>
     </ThemeProvider>
   );
